afa294ea32848a4d647242fe1876c963adc13dfb
Himanshu Sinha <ashusinha1904@gmail.com>
2024-08-16 22:57:10 +0530

    completed page UI completed

--- app/(main)/completed.tsx
+++ app/(main)/completed.tsx
@@ -1,11 +1,24 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, FlatList } from "react-native";
 import React from "react";
 import tw from "twrnc";
 
 import SafeView from "@/components/SafeView";
 import Progress from "@/components/Progress";
+import TodoCard from "@/components/TodoCard";
 
 const Completed = () => {
+  const dummyTodos = [
+    {
+      title: "Random Task",
+      startDate: "16 August 2024",
+      endDate: "20 August 2024",
+    },
+    {
+      title: "Random Task 2",
+      startDate: "16 August 2024",
+      endDate: "18 August 2024",
+    },
+  ];
   return (
     <SafeView>
       <ScrollView showsVerticalScrollIndicator={false}>
@@ -13,8 +26,14 @@ const Completed = () => {
           Completed
         </Text>
 
-        <View style={tw`items-center px-4 mt-8 gap-y-8`}>
+        <View style={tw`w-full items-center px-4 mt-8 gap-y-8`}>
           <Progress />
+
+          <View style={tw`w-full gap-y-4`}>
+            {dummyTodos.map((todo) => {
+              return <TodoCard key={todo.title} todo={todo} />;
+            })}
+          </View>
         </View>
       </ScrollView>
     </SafeView>
