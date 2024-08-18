0d6a76e78a6bba237a6ea09c6b82863b269079db
Himanshu Sinha <ashusinha1904@gmail.com>
2024-08-18 20:04:32 +0530

    New page all-todos added

---
+++ app/(others)/all-todos.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import React from "react";
+import tw from "twrnc";
+
+import SafeView from "@/components/SafeView";
+
+const AllTodos = () => {
+  return (
+    <SafeView>
+      <Text>AllTodos</Text>
+    </SafeView>
+  );
+};
+
+export default AllTodos;
