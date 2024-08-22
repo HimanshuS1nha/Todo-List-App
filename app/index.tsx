47441d7a111ece1487d2e94c1793ed36b32c6e70
Himanshu Sinha <ashusinha1904@gmail.com>
2024-08-22 15:24:37 +0530

    function to fetch todos added

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
 import { router, useRootNavigationState } from "expo-router";
 import { useEffect } from "react";
 import { Text, View } from "react-native";
+import { useQuery } from "@tanstack/react-query";
+import { useSQLiteContext } from "expo-sqlite";
+
+import { useTodos } from "@/hooks/useTodos";
 
 export default function Index() {
   const rootNavigationState = useRootNavigationState();
+  const db = useSQLiteContext();
+  const { getTodos } = useTodos();
+
+  const { data } = useQuery({
+    queryKey: ["get-todos"],
+    queryFn: () => getTodos(db),
+  });
 
   useEffect(() => {
     if (rootNavigationState?.key) {
-      router.replace("/pending");
+      if (data) {
+        router.replace("/pending");
+      }
     }
-  }, [rootNavigationState?.key]);
+  }, [rootNavigationState?.key, data]);
   return (
     <View
       style={{
--- hooks/useTodos.ts
+++ hooks/useTodos.ts
@@ -1,3 +1,4 @@
+import { SQLiteDatabase } from "expo-sqlite";
 import { create } from "zustand";
 
 type TodoType = {
@@ -12,9 +13,21 @@ type TodoType = {
 type UseTodosType = {
   todos: TodoType[];
   setTodos: (todos: TodoType[]) => void;
+  getTodos: (db: SQLiteDatabase) => Promise<boolean>;
 };
 
 export const useTodos = create<UseTodosType>((set) => ({
   todos: [],
   setTodos: (todos) => set({ todos }),
+  getTodos: async (db) => {
+    try {
+      const todos = await db.getAllAsync<TodoType>(
+        "SELECT * from todos ORDER BY id"
+      );
+      set({ todos });
+      return true;
+    } catch (error) {
+      return false;
+    }
+  },
 }));
