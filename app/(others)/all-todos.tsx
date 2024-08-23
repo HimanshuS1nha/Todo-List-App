ce1096cfdbcab9a1a5653d6b1df57194c675df8d
Himanshu Sinha <ashusinha1904@gmail.com>
2024-08-23 22:49:18 +0530

    Todo search functionality added

--- app/(others)/all-todos.tsx
+++ app/(others)/all-todos.tsx
@@ -1,5 +1,5 @@
-import { View, Text, ScrollView } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, TextInput } from "react-native";
+import React, { useCallback, useState } from "react";
 import tw from "twrnc";
 
 import SafeView from "@/components/SafeView";
@@ -9,17 +9,46 @@ import { useTodos } from "@/hooks/useTodos";
 
 const AllTodos = () => {
   const { todos } = useTodos();
+
+  const [filteredTodos, setFilteredTodos] = useState(todos);
+
+  const filterTodos = useCallback(
+    (searchQuery: string) => {
+      if (searchQuery === "") {
+        if (filteredTodos === todos) {
+          return;
+        } else {
+          return setFilteredTodos(todos);
+        }
+      }
+
+      const newTodos = todos.filter((todo) =>
+        todo.title.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+
+      setFilteredTodos(newTodos);
+    },
+    [todos, filteredTodos]
+  );
   return (
     <SafeView>
       <Header title="All Tasks" />
 
+      <View style={tw`items-center mb-6`}>
+        <TextInput
+          style={tw`bg-white shadow shadow-black border border-gray-300 w-[90%] py-2.5 px-3 rounded-xl`}
+          placeholder="Search..."
+          onChangeText={filterTodos}
+        />
+      </View>
+
       <ScrollView contentContainerStyle={tw`px-4 gap-y-4`}>
-        {todos.length === 0 && (
+        {filteredTodos.length === 0 && (
           <Text style={tw`text-rose-600 font-semibold text-base text-center`}>
             No Data to show.
           </Text>
         )}
-        {todos.map((todo) => {
+        {filteredTodos.map((todo) => {
           return <TodoCard key={todo.title} todo={todo} />;
         })}
       </ScrollView>
